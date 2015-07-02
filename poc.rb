e268bd4531211b19fa22297b54309c207d11d4f8
karma <boris.isolet@gmail.com>
2015-07-02 15:11:18 +0800

    for debug

--- poc.rb
+++ poc.rb
@@ -14,6 +14,32 @@ def load_csv(file)
   ary
 end
 
+def to_csv(file, rows, title = nil, encode = "utf-8")
+  if File.exist?(file)
+    file = file.gsub(".csv", "_new.csv")
+  end
+  CSV.open(file, "wb") do |csv|
+    if title.is_a? Array
+      csv << title
+    elsif title == true
+      title = rows.shift
+    end
+    if encode == "utf-8"
+      csv << title if title 
+      rows.each{ |row| csv << (block_given? ? yield(row) : row) }
+    else
+      csv << title.map!{|str| str.to_s.encode(encode, 'utf-8',{:invalid => :replace, :undef => :replace, :replace => '?'}) } if title  
+      rows.each do |row| 
+        if block_given? 
+          csv << yield(row).each{|str| str.to_s.encode('gbk','utf-8',{:invalid => :replace, :undef => :replace, :replace => '?'}) }
+        else 
+          csv << row.each{|str| str.to_s.encode('gbk','utf-8',{:invalid => :replace, :undef => :replace, :replace => '?'}) }
+        end 
+      end
+    end
+  end
+end
+
 ARGV.each do |file|
   ary = load_csv(file)
   to_csv(file.gsub('.csv', "_mr.csv"), ary, false)
