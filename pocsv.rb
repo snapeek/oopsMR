0b55bc3f03bffafb6669f7b05999504aa0d9f8ed
karma <boris.isolet@gmail.com>
2015-07-17 13:05:58 +0800

    added web api

---
+++ pocsv.rb
@@ -0,0 +1,61 @@
+require 'timeout'
+def to_csv(file, rows, title = nil, encode = "utf-8")
+  if File.exist?(file)
+    file = file.gsub(".csv", "_new.csv")
+  end
+  CSV.open(file, "wb") do |csv|
+    if title.is_a? Array
+      csv << title
+    elsif title == true
+      title = rows.shift
+    end
+    if encode == "utf-8"
+      csv << title if title 
+      rows.each{ |row| csv << (block_given? ? yield(row) : row) }
+    else
+      csv << title.map!{|str| str.to_s.encode(encode, 'utf-8',{:invalid => :replace, :undef => :replace, :replace => '?'}) } if title  
+      rows.each do |row| 
+        if block_given? 
+          csv << yield(row).each{|str| str.to_s.encode('gbk','utf-8',{:invalid => :replace, :undef => :replace, :replace => '?'}) }
+        else 
+          csv << row.each{|str| str.to_s.encode('gbk','utf-8',{:invalid => :replace, :undef => :replace, :replace => '?'}) }
+        end 
+      end
+    end
+  end
+  file
+end
+
+def to_encode
+  CSV.open(file, "r") do |csv|
+    CSV.open(file.gsub('.csv', '_gbk.csv'), "wb") do |csvw|
+      while line = csv.readline
+        csvw << line.map { |str| str.to_s.encode('gbk','utf-8',{:invalid => :replace, :undef => :replace, :replace => '?'}) }
+      end
+    end
+  end  
+end
+
+def load_ori(file, &block)
+  ary = []
+  i = 0
+  CSV.open(file, "r") do |csv|
+    while line = csv.readline
+      i += 1
+      begin
+        Timeout::timeout(5) {
+          line.map!{|str| str.to_s.encode('utf-8','utf-8',{:invalid => :replace, :undef => :replace, :replace => ''})}
+          ary << block.call(line)
+        }
+        puts "poc on line #{i}" if i % 5 == 0
+      rescue Timeout::Error
+        puts "timeout on line #{i}"
+        ary << line
+      rescue Exception => e
+        puts "error on line #{i}"
+        ary << line
+      end
+    end
+  end
+  ary
+end
--- web.rb
+++ web.rb
@@ -1,17 +1,58 @@
 require 'sinatra'
+require "pry"
+require 'csv'
 
-get :index do
-  
+require './sad_panda'
+require './nlpir'
+require "./pocsv"
+
+$s = SadPanda.new
+nlpir_init(UTF8_CODE)
+
+configure do
+  set :public_folder, "#{File.dirname(__FILE__)}/assets"
+  set :views, "#{File.dirname(__FILE__)}/views"
+  set :show_exceptions, :after_handler
 end
 
-get :sentiment do
-  
+configure :production, :development do
+  enable :logging
 end
 
-get :to_utf8 do
-  
+get "/" do
+  # @profiles = Profile.all
+  erb :form
 end
 
-get :to_gbk do
-  
+post "/upload" do
+  binding.pry
+  unless params[:file] &&  
+      (tempfile = params[:file][:tempfile]) &&  
+      (filename = params[:file][:filename])  
+    @error = 'No file selected'  
+    redirect to('/')  
+  end
+  target = "./files/#{filename}"  
+  File.open(target, 'wb') {|f| f.write tempfile.read }
+  poc = select_poc(params[:poc], params[:n].to_i)
+  csv = load_ori(target, poc)
+  to_csv(csv)
+end
+
+
+def select_poc(poc, n = 0)
+  case poc
+  when "f"
+    ->(line){
+      line << text_proc(line[n])
+    }
+  when "s"
+    ->{
+      line << $s.start(text_proc(line[n]))
+    }
+  when "g"
+    ->{
+      line << line.map { |str| str.to_s.encode('gbk','utf-8',{:invalid => :replace, :undef => :replace, :replace => ''})}
+    }
+  end
 end
