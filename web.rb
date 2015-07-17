6c0e097cdef1c750ea6a627649650f73296ce3e7
karma <boris.isolet@gmail.com>
2015-07-17 15:36:37 +0800

    for debug

--- web.rb
+++ web.rb
@@ -11,7 +11,7 @@ $s = SadPanda.new
 nlpir_init(UTF8_CODE)
 
 configure do
-  set :public_folder, "#{File.dirname(__FILE__)}/files"
+  # set :public_folder, "#{File.dirname(__FILE__)}/files"
   set :views, "#{File.dirname(__FILE__)}/views"
   set :show_exceptions, :after_handler
   set :environment, :production
@@ -38,11 +38,7 @@ post "/sent" do
   poc = select_poc(params[:poc], params[:n].to_i)
   csv = load_ori(target, poc)
   new_file_name = to_csv(target ,csv)
-  # binding.pry
   send_file(new_file_name, :type => "text/csv")
-  #response.headers['content_type'] = "application/octet-stream"
-  #attachment(new_file_name)
-  #response.write(File.read(new_file_name, 'r'))  
 end
 
 
