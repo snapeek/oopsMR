a799de30e6bafd3e3c3af85864c4e57d3b5af955
karma <boris.isolet@gmail.com>
2015-07-18 14:45:00 +0800

    for debug

--- pocsv.rb
+++ pocsv.rb
@@ -2,8 +2,8 @@ require 'pry'
 def to_csv(file, rows, encode = "utf-8")
   file = file.gsub(".csv", "_new.csv") if File.exist?(file)
   CSV.open(file, "wb") do |csv|
-    binding.pry
-    title = ((rows.shift)[-1] = "感情色彩") 
+    title = rows.shift
+    title[-1] = "感情色彩"
     csv << title.map{|t| en(t, encode)}
     rows.each do |row| 
       csv << row.map{|r| en(r, encode)}
